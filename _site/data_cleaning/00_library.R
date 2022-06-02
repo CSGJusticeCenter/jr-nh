5a5887bb8a80e5eae367859ddc44f2939f6ce4d6
mrobertsCSG <mroberts@csg.org>
2022-06-02 15:59:15 -0400

    carroll county data cleaning

--- _site/data_cleaning/00_library.R
+++ _site/data_cleaning/00_library.R
@@ -1,10 +1,26 @@
 ############################################
-# Packages
-# Last updated: May 16, 2022
+# Project: JRI New Hampshire
+# File: library.R
+# Last updated: June 1, 2022
 # Author: Mari Roberts
+
+# Load packages
 ############################################
 
+###################
+# CHANGE THESE DEPENDING ON USER
+###################
+
+# path to jail data on research division sharepoint
+CSG_SP_PATH = "C:/Users/mroberts/The Council of State Governments/JC Research - JR_NH"
+
+# path to local environment
+L_PATH = "C:/Users/mroberts/OneDrive - The Council of State Governments/Desktop/csgjc/repos/jr-nh"
+
+###################
 # load packages
+###################
+
 library(readxl)
 library(readr)
 library(janitor)
@@ -16,6 +32,7 @@ library(ggplot2)
 library(dplyr)
 library(kableExtra)
 library(stringr)
+library(csgjcr)
 
 # highchart
 library(highcharter)
--- _site/data_cleaning/02_import.R
+++ _site/data_cleaning/02_import.R
@@ -1,30 +1,76 @@
 ############################################
-# Import Data
+# Project: JRI New Hampshire
+# File: import.R
 # Last updated: May 12, 2022
 # Author: Mari Roberts
 
 # Imports files from jail partners
 ############################################
 
+# load packages
 source("data_cleaning/00_library.R")
+
+###################
+# Belknap County
+###################
+
+# set working directory
+setwd(CSG_SP_PATH)
+belknap.xlsx <- read_csv("Data/Belknap County/Jail History Admin Form - Belknap DOC.csv")
 
 ###################
 # Carroll County
 ###################
 
-carroll_releases.xlsx <- read_excel("C:/Users/mroberts/The Council of State Governments/JC Research - JR_NH/Data/Carroll County/Release data CSG.xls")
-carroll_bookings.xlsx <- read_excel("C:/Users/mroberts/The Council of State Governments/JC Research - JR_NH/Data/Carroll County/CSG booking data edited.xls")
+carroll_releases.xlsx <- read_excel("Data/Carroll County/CSG release data 7.1.2018-6.30.2021.xls")
+carroll_bookings.xlsx <- read_excel("Data/Carroll County/CSG data 7.1.2018-6.30-2021.xls")
 
 ###################
 # Cheshire County
 ###################
 
-cheshire_adm.xlsx      <- read_excel("C:/Users/mroberts/The Council of State Governments/JC Research - JR_NH/Data/Cheshire County/CSG Jail Data D-IDENT.xlsx")
-cheshire_prog.xlsx     <- read_excel("C:/Users/mroberts/The Council of State Governments/JC Research - JR_NH/Data/Cheshire County/MRT Attendance Logs CSG.xlsx")
-cheshire_mh_stats.xlsx <- read_excel("C:/Users/mroberts/The Council of State Governments/JC Research - JR_NH/Data/Cheshire County/AAA MH & SU STATS CSG.xls")
+cheshire_adm.xlsx      <- read_excel("Data/Cheshire County/CSG Jail Data D-IDENT.xlsx")
+cheshire_prog.xlsx     <- read_excel("Data/Cheshire County/MRT Attendance Logs CSG.xlsx")
+cheshire_mh_stats.xlsx <- read_excel("Data/Cheshire County/AAA MH & SU STATS CSG.xls")
+
+###################
+# Coos County
+###################
+
+
+###################
+# Grafton County
+###################
+
+
+###################
+# Hillsborough County
+###################
+
 
 ###################
 # Merrimack County
 ###################
 
-merrimack_adm.xlsx     <- read_excel("C:/Users/mroberts/The Council of State Governments/JC Research - JR_NH/Data/Merrimack County/Data Field 2 CSG 2019 to 2021 (2).xlsx")
+merrimack_adm.xlsx <- read_excel("Data/Merrimack County/Data Field 2 CSG 2019 to 2021 (2).xlsx")
+
+###################
+# Rockingham County
+###################
+
+
+###################
+# Strafford County
+###################
+
+
+###################
+# Sullivan County
+###################
+
+
+###################
+# Set working directory
+###################
+
+setwd(L_PATH)
