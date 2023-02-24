054f4b7e6a6be8e7f400caa401fd583d99cfb726
mrobertsCSG <mroberts@csg.org>
2023-02-24 16:21:29 -0500

    update colors

--- data_cleaning/00_library.R
+++ data_cleaning/00_library.R
@@ -85,10 +85,9 @@ default_fonts <- c("Franklin Gothic Book")
 ###################
 
 # Official jri colors
-jri_light_blue <- "#4095B1"
-jri_dark_blue  <- "#273C4C"
+jri_light_blue <- "#167a9c"
+jri_dark_blue  <- "#293e5c"
 jri_red        <- "#E25449"
-jri_green      <- "#61C280"
-jri_off_green  <- "#779F38"
-jri_orange     <- "#E17619"
+jri_green      <- "#557e39"
+jri_orange     <- "#b95826"
 
